0207f489507d37b8dd1ec9be94b08a9b9fe0dfdf
Manfred Stienstra <manfred@fngtps.com>
2009-12-04 18:16:25 +0100

    Add documentation for the FakutoriSan model and the Fakutori method.

--- lib/fakutori_san.rb
+++ lib/fakutori_san.rb
@@ -1,14 +1,12 @@
 require 'fakutori_san/fakutori'
 
-# = FakutoriSan
-#
-# As its description, FakutoriSan aims to be a lean model factory. As it uses
-# vanilla Ruby to define the factories, you can use class inheritance and all
-# other standard Ruby practices.
+# FakutoriSan is the module where most of the implementation resides.
 module FakutoriSan
 end
 
 module Kernel
+  # The Fakutori method is used to instantiate your factories. For more information about defining
+  # and using factories see FakutoriSan::Fakutori and the examples.
   def Fakutori(model)
     FakutoriSan.factories[model] || raise(FakutoriSan::FakutoriMissing, "No factory defined for model `#{model}'")
   end
